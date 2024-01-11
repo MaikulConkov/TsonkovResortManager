d81087cf7f58410410668d89638d68b09f945d96
MaikulConkov <mcjaiyo01@gmail.com>
2024-01-11 12:49:54 +0300

    Initial Commit

---
+++ src/test/java/com/example/TsonkovResortManager/TsonkovResortManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TsonkovResortManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsonkovResortManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
